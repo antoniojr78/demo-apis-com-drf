820ae30291b521292fe469c97bc09ca6871c5951
antoniojr <antonio.silva@wikki.com.br>
2020-01-22 13:57:45 -0300

    Adding decorators for field Birth year.

--- my_project/students/models.py
+++ my_project/students/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
 
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
     profession = models.CharField(max_length=200)
-    birth_year = models.PositiveSmallIntegerField()
+    birth_year = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1900), 
+        MaxValueValidator(datetime.now().year)])
     active = models.BooleanField(default=True)
 
     class Meta:
         db_table = "Student"
 
     def __str__(self):
-        return int(self.id) + ' - ' + self.name
+        return str(self.id) + ' - ' + self.name
