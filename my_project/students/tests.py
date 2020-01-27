6a7a4439c5369e49cd7f44d1a59c97ee2b86a7ca
antoniojr <antonio.silva@wikki.com.br>
2020-01-27 18:33:14 -0300

    Creating test for endpoint /api/students/ with Get method.

--- my_project/students/tests.py
+++ my_project/students/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from rest_framework import status
 
-# Create your tests here.
+'''https://docs.djangoproject.com/en/2.1/topics/testing/overview/
+python my_project/manage.py test students
+python my_project/manage.py test students.tests
+python my_project/manage.py test students.tests.StudentsTests
+python my_project/manage.py test students.tests.StudentsTests.test_get_students
+'''
+
+
+class StudentsTests(TestCase):
+    def test_get_students(self):
+        client = Client()
+        response = client.get('/api/students/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
