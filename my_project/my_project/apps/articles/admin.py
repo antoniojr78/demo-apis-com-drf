b7f4a4a2e658d295930fc515ca66460c0d1f2cbd
antoniojr <antonio.silva@wikki.com.br>
2020-01-28 11:12:53 -0300

    Customizing Article App in Admin.

--- my_project/my_project/apps/articles/admin.py
+++ my_project/my_project/apps/articles/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Author, Article
 
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'insert_date', 'update_date')
+
+    class Meta:
+        model = Article
+
+
 admin.site.register(Author)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
