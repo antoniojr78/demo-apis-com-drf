6af928b17a898801492a76e6c9409a97f8a038b9
antoniojr <antonio.silva@wikki.com.br>
2020-01-28 10:30:52 -0300

    Renaming Author Class.

--- my_project/my_project/apps/articles/admin.py
+++ my_project/my_project/apps/articles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Actor
+from .models import Author
 
 
-admin.site.register(Actor)
+admin.site.register(Author)
 
--- my_project/my_project/apps/articles/models.py
+++ my_project/my_project/apps/articles/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class Actor(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=150)
     address = models.CharField(max_length=255)
     site = models.URLField(blank=True, null=True)
     email = models.EmailField()
 
     class Meta:
-        db_table = 'actor'
+        db_table = 'author'
 
     def __str__(self):
         return self.name + ' - ' + self.email
