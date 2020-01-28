c461e7712d33bf9fab4d33b08273d37e7014e900
antoniojr <antonio.silva@wikki.com.br>
2020-01-28 10:24:40 -0300

    Creating Article Author model.

--- my_project/my_project/apps/articles/models.py
+++ my_project/my_project/apps/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=255)
+    site = models.URLField(blank=True, null=True)
+    email = models.EmailField()
+
+    class Meta:
+        db_table = 'actor'
+
+    def __str__(self):
+        return self.name + ' - ' + self.email
